2fe87d001ab5a6fa44ee9c30bf6a3b0cb4bfa7c5
Your Name <you@example.com>
2024-06-20 12:59:44 +0530

    ReverseInteger.py

--- Mathematical_Problems/ReverseInteger.py
+++ Mathematical_Problems/ReverseInteger.py
@@ -2,26 +2,30 @@
 https://leetcode.com/problems/reverse-integer/
 '''
 
+import math
 
-def reverse(x):
-
+def reverse(x) -> int:
     # flag var to check '-ve' or '+ve'
-    sign = -1 if x<0 else 1
+    sign = 1
+    if x < 0:
+        sign = -1
+        x = -1 * x
 
     # reverse number store variable
     revNum = 0
 
     # iterate till x == 0
-    while x!=0:
-        revNum = revNum*10 + x%10
-        x//=10
+    while x > 0:
+        revNum = revNum * 10 + x % 10
+        x //= 10
 
     # return reverse number along with sign
-    return sign*revNum
+    ans = sign * revNum
+    return ans if (ans >= -1 * math.pow(2, 31) and ans <= math.pow(2, 31) - 1) else 0
 
 
 if __name__ == '__main__':
-    print(reverse(1001))
+    print(reverse(1534236))
 
 
 
